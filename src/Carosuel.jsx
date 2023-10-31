888adf1b86215bc37fefb69b4432083282c1f571
sunny patel <sunnykpatel.skp@gmail.com>
2023-10-31 12:35:53 -0400

    add slider button functionality

--- src/Carosuel.jsx
+++ src/Carosuel.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { shortList } from './data';
+import { list, shortList } from './data';
 import {
   FaChevronCircleLeft,
   FaChevronCircleRight,
@@ -7,13 +7,40 @@ import {
 } from 'react-icons/fa';
 
 const Carosuel = () => {
-  const [people, setPeople] = useState(shortList);
+  const [people, setPeople] = useState(list);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const prevSlide = () => {
+    setCurrentIndex((old) => {
+      let newIndex = old - 1;
+      if (newIndex < 0) {
+        return (newIndex = people.length - 1);
+      }
+      return newIndex;
+    });
+  };
+  const nextSlide = () => {
+    setCurrentIndex((old) => {
+      let newIndex = old + 1;
+      if (newIndex > people.length - 1) {
+        return (newIndex = 0);
+      }
+      return newIndex;
+    });
+  };
+
   return (
     <div className="slider-container">
-      {people.map((person) => {
+      {people.map((person, personIndex) => {
         const { id, image, name, quote, title } = person;
         return (
-          <div className="slide" key={id}>
+          <div
+            className="slide"
+            key={id}
+            style={{
+              transform: `translateX(${100 * (personIndex - currentIndex)}%)`,
+            }}
+          >
             <img src={image} alt={name} className="person-img" />
             <h2 className="name">{name}</h2>
             <h3 className="title">{title}</h3>
@@ -22,10 +49,10 @@ const Carosuel = () => {
           </div>
         );
       })}
-      <button type="button" className="prev">
+      <button type="button" className="prev" onClick={prevSlide}>
         <FaChevronCircleLeft />
       </button>
-      <button type="button" className="next">
+      <button type="button" className="next" onClick={nextSlide}>
         <FaChevronCircleRight />
       </button>
     </div>
